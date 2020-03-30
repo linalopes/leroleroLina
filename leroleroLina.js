1a67362258e2fdce74c97835ab07f3388a845258
Lina Lopes <lina@lilo.zone>
2020-03-30 11:18:09 -0300

    Add files via upload

---
+++ leroleroLina.js
@@ -0,0 +1,49 @@
+var lines, markov, data1, data2, 
+    x = 360, y = 300;
+var lero;
+
+function preload() {
+  data = loadStrings('data/lina.txt');
+}
+ 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textFont('prompt', 16);
+  textAlign(LEFT);
+
+  lines = ["clique para (re)gerar!"];
+
+  // create a markov model w' n=4
+  markov = new RiMarkov(4);
+
+  // load text into the model
+  markov.loadText(data.join(' '));
+  
+
+  drawText();
+}
+
+
+function drawText() {
+  background(221, 50, 36);
+  fill(240, 238, 243);
+  text(lines.join(' '), x, y, width/2, height/2);
+  lero = lines.join('');
+}
+
+function mouseClicked() {
+  x = y = 50;
+  lines = markov.generateSentences(10);
+  drawText();
+}
+
+function keyPressed() {
+  if (key == 's') {
+    // creates a file called 'newFile.txt'
+    let writer = createWriter('lerolero.txt');
+    // write 'Hello world!'' to the file
+    writer.write(lero);
+    // close the PrintWriter and save the file
+    writer.close();
+  }
+}
